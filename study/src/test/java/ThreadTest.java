53bbfc4f33282056b499fadf87e330f9c5eca25b
刘金星 <821901008@qq.com>
2021-04-10 23:53:49 +0800

    feat: 线程处理

---
+++ study/src/test/java/ThreadTest.java
@@ -0,0 +1,19 @@
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+public class ThreadTest {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();
+        System.out.println("hello");
+    }
+}
