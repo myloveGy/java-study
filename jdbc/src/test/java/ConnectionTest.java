8ccd1820ec7f54fc16b5e97387c65d0fb2d45d66
刘金星 <821901008@qq.com>
2021-05-18 11:39:48 +0800

    feat: jdbc 学习

--- jdbc/src/test/java/ConnectionTest.java
+++ jdbc/src/test/java/ConnectionTest.java
@@ -1,14 +1,16 @@
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.Driver;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionTest {
 
     @Test
-    public void testConnection() throws SQLException {
+    public void testConnection1() throws SQLException {
         Driver driver = new com.mysql.jdbc.Driver();
 
         Properties configInfo = new Properties();
@@ -20,7 +22,39 @@ public class ConnectionTest {
     }
 
     @Test
-    public void test() {
+    public void testConnection2() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, SQLException {
+        // 1、获取Driver实现类：使用反射
+        Class driverClass = Class.forName("com.mysql.jdbc.Driver");
+        Driver driver = (Driver) driverClass.getDeclaredConstructor().newInstance();
+        Properties configInfo = new Properties();
+        configInfo.setProperty("user", "root");
+        configInfo.setProperty("password", "123456");
 
+        Connection connection = driver.connect("jdbc:mysql://localhost:3306/java-study", configInfo);
+        System.out.println(connection);
+    }
+
+    @Test
+    public void testConnection3() throws Exception {
+        // 1、获取Driver实现类：使用反射
+        Class driverClass = Class.forName("com.mysql.jdbc.Driver");
+        Driver driver = (Driver) driverClass.getDeclaredConstructor().newInstance();
+
+        // 2、注册驱动
+        DriverManager.registerDriver(driver);
+
+        // 3、获取连接
+        Connection root = DriverManager.getConnection("jdbc:mysql://localhost:3306/java-study", "root", "123456");
+        System.out.println(root);
+    }
+
+    @Test
+    public void testConnection4() throws Exception {
+        // 1、获取Driver实现类：使用反射
+        Class.forName("com.mysql.jdbc.Driver");
+
+        // 2、获取连接
+        Connection root = DriverManager.getConnection("jdbc:mysql://localhost:3306/java-study", "root", "123456");
+        System.out.println(root);
     }
 }
